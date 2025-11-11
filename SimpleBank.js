632c0d3f8fd04187941a1de209ba05bff0f6dab2
NathanDev <jordynathandgjgh@gmail.com>
2025-11-11 01:50:43 +0100

    Refactor SimpleBank class for account operations
    Fix syntax errors and improve account management methods.

--- SimpleBank.js
+++ SimpleBank.js
@@ -1,1 +1,34 @@
+class SimpleBank {
+    constructor() {
+        this.accounts = {};
+    }
+
+  createAccount(accountId) {
+    if (this.accounts[accountId] {
+      throw new Error ('Account already exsts'); 
+      }
+        this.accounts[accountId] = 0;
+    }
+
+  deposit(accountId, amount) {
+        if (!this.accounts[accountId]) {
+            throw new Error('Account does not exist');
+        }
+        if (amount <= 0) {
+            throw new Error('Deposit amount must be positive');
+        }
+        this.accounts[accountId] += amount;
+    }
+
+  withdraw(accountId, amount) {
+        if (!this.accounts[accountId]) {
+            throw new Error('Account does not exist');
+        }
+        if (amount <= 0) {
+            throw new Error('Withdrawal amount must be positive');
+        }
+
+      }
+        
+}       
 
